667f47d99f520bec9235abcc6aa89d261cafd973
bonvi83 <bonvi83@gmail.com>
2024-01-16 17:15:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// ESERCIZIO 01
+
+// let a = "montagna";
+// console.log(a);
+
+// let b = "mare";
+// console.log(b);
+
+// a = b;
+// b = c;
+
+
+
+// ESERCIZIO 02
+
+// let ramo = "";
+
+// for (let i = 0; i < 6; i++){
+
+//     ramo = ramo + "*";
+//     console.log(ramo);
+// }
+
+// const tronco = "||";
+// console.log(tronco);
+
+// const radici = "```";
+// console.log(radici);
+
+
+
+// ESERCIZIO 03: 
+
+// const numeri = [4, 5, 3,1, 11, 21, 23, 0 , 2, 44, 19, 99, 100, 2, 3, 1];
+
+// const pari = [];
+// const dispari = [];
+
+// for (let i = 0; i < numeri.length; i++) {
+//     if (numeri[i] % 2 == 0) {
+//         pari.push(numeri[i]);
+//     } else {
+//         dispari.push(numeri[i]);
+//     }
+// }
+
+
+
+// ESERCIZIO 04
+
+
+
+
+// ESERCIZIO 05
+
+// for (let i = 10; i >= 0; i--){
+
+//     console.log(i)
+// }
+
+
+
+// let i = 10;
+
+// while (i >= 0){
+//     console.log(i)
+
+//     i--;
+// }
